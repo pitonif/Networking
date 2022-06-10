84f4456b54ee69ee7d53378481dfcc946a857789
Lobachkov Oleg <olobachkov@gmail.com>
2022-06-10 16:06:52 +0300

    start project

---
+++ Networking2/Networking2/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Networking2
+//
+//  Created by Oleg on 10.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
